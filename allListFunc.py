e6a1b56f67b1893dfbb44920ca05cd533702d147
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-17 19:50:26 +0600

    Python Function Of List

---
+++ allListFunc.py
@@ -0,0 +1,11 @@
+list.append(x) # append x to end of list
+list.extend(iterable) # append all elements of iterable to list
+list.insert(i, x) # insert x at index i
+list.remove(x) # remove first occurance of x from list
+list.pop([i]) # pop element at index i (defaults to end of list)
+list.clear() # delete all elements from the list
+list.index(x[, start[, end]]) # return index of element x
+list.count(x) # return number of occurances of x in list
+list.reverse() # reverse elements of list in-place (no return)
+list.sort(key=None, reverse=False) # sort list in-place
+list.copy() # return a shallow copy of the list

232bcc0b1268da443da3ff7965db4cfc8c4ad6d2
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-17 19:44:58 +0600

    Taking Three Inout at a Time

---
+++ takingThreeInput.py
@@ -0,0 +1,6 @@
+# taking three inputs at a time
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+print()

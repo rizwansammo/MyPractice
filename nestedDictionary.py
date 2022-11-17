56a300f2c968c84a4642b88a611f26f7b044107d
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-17 19:59:45 +0600

    Python Nested Dictionary

---
+++ nestedDictionary.py
@@ -0,0 +1,14 @@
+myfamily = {
+  "child1" : {
+    "name" : "Rizwan",
+    "year" : 2000
+  },
+  "child2" : {
+    "name" : "Sammo",
+    "year" : 2000
+  },
+  "child3" : {
+    "name" : "Zero",
+    "year" : 2011
+  }
+}
